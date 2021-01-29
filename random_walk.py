114185dd3a49692d15256cb1c94d0f6535613ccb
Karen Luong <karenmluong@gmail.com>
2021-01-29 18:36:28 +0000

    animated the walker!!

--- random_walk.py
+++ random_walk.py
@@ -51,54 +51,26 @@ class RandomWalk:
 
 
 if __name__ == "__main__":
-    walk = RandomWalk(1,0,10,30,5,55)
-    walk.walk()
+    walk = RandomWalk(0,0,25,25,25,25)
+    walk.walk(10000)
     trail = walk.trail
     # First set up the figure, the axis, and the plot element we want to animate
-    #fig = plt.figure()
-    #ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
-    #line, = ax.plot([], [], lw=2)
-
-    # initialization function: plot the background of each frame
-    #def init():
-    #    line.set_data([trail[0][0]],[trail[0][1]])
-    #    return line,
-
-    # animation function.  This is called sequentially
-    #def animate(i):
-    #    line.set_data([trail[i][0]],[trail[i][1]])
-    #    return line,
-
-    # call the animator.  blit=True means only re-draw the parts that have changed.
-    #anim = animation.FuncAnimation(fig, animate, init_func=init,
-    #                               frames=10, interval=20, blit=True)
-    #plt.show()
-    # First set up the figure, the axis, and the plot element we want to animate
     fig = plt.figure()
-    ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
-    line, = ax.plot([], [], lw=2)
-
+    ax = plt.axes(xlim=(-50, 50), ylim=(-50, 50))
+    line, = ax.plot([], [], lw=1)
+    x_cords = [trail[i][0] for i in range(len(trail))]
+    y_cords = [trail[i][1] for i in range(len(trail))]
     # initialization function: plot the background of each frame
     def init():
-        line.set_data([], [])
+        line.set_data([x_cords[0]],[y_cords[0]])
         return line,
 
     # animation function.  This is called sequentially
     def animate(i):
-        x = np.linspace(0, 2, 1000)
-        y = np.sin(2 * np.pi * (x - 0.01 * i))
-        line.set_data(x, y)
+        line.set_data(x_cords[0:i],y_cords[0:i])
         return line,
 
     # call the animator.  blit=True means only re-draw the parts that have changed.
     anim = animation.FuncAnimation(fig, animate, init_func=init,
-                                   frames=200, interval=20, blit=True)
-
-    # save the animation as an mp4.  This requires ffmpeg or mencoder to be
-    # installed.  The extra_args ensure that the x264 codec is used, so that
-    # the video can be embedded in html5.  You may need to adjust this for
-    # your system: for more information, see
-    # http://matplotlib.sourceforge.net/api/animation_api.html
-    #anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
-
+                                   frames=10000, interval=100, blit=True)
     plt.show()
